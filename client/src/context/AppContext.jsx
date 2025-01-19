b970144ab22c9f04f4c5ed710e906d3d9c769fb3
Tejasvini Kumawat <tejasvinikumawat2004@gmail.com>
2025-01-19 19:11:49 +0530

    Application page done

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+import { createContext } from 'react'
+import { jobsData } from '../assets/assets'
+
+ export const AppContext = createContext()
+export const AppContextProvider=(props)=>{
+
+    const [searchFilter,setSearchFilter]=useState({
+      title:' ',
+      location:' '
+    })
+    const [isSearched,setIsSearch]=useState(false)
+    const [jobs,setJobs]=useState([])
+
+    //function to fetch job data
+    const fetchJobs =async () =>{
+setJobs(jobsData)
+    }
+    useEffect(()=>{
+fetchJobs()
+    },[])
+    const value={
+        searchFilter,setSearchFilter,
+        isSearched,setIsSearch,
+        jobs,setJobs
+    }
+    return (<AppContext.Provider value={value}>
+{props.children}
+    </AppContext.Provider>
+
+    )
+}
